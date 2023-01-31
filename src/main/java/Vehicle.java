c27837c40f127a7c4f168f1acbcda187152c4ba1
valfink <valentin.finke@gmail.com>
2023-01-31 15:37:32 +0100

    Add abstract Class Vehicle

---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,29 @@
+public abstract class Vehicle implements Acceleratable {
+    private int id;
+    private String name;
+    private int velocity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+}
